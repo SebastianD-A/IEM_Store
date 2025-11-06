b9978a1aaee66b2337db92af7d4dee3d000c8e8f
Seb <sebastian.arifin@protonmail.com>
2025-11-06 21:48:20 +0700

    Created empty classes

--- Stuff.java
+++ Stuff.java
@@ -3,3 +3,49 @@ package IEM_Store;
 public class Stuff {
     
 }
+
+//enums
+enum Drivers{
+    DYNAMIC,
+    DUAL_DYNAMIC,
+    PLANAR,
+    BA,
+    BA_DD_HYBRID,
+    TRIBID,
+    QUADBRID,
+}
+//interfaces
+interface Sell{
+    double applyDiscount(double percentage);
+    void displayInfo();
+}
+//classes
+class Product implements Sell{
+    public double applyDiscount(double percentage){
+        return 0;
+    }
+
+    public void displayInfo(){
+        
+    }
+}
+
+class InEarMonitor extends Product{
+
+}
+
+class Accessory extends Product{
+
+}
+
+class Customer{
+
+}
+
+class Order{
+
+}
+
+class Store{
+
+}
