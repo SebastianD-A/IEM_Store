ee4e41a920a21426488ffe396f054c5e7a1ce776
Seb <sebastian.arifin@protonmail.com>
2025-11-08 23:56:10 +0700

    created basic functions

--- Stuff.java
+++ Stuff.java
@@ -1,11 +1,12 @@
 package IEM_Store;
+import java.util.*;
 
 public class Stuff {
     
 }
 
 //enums
-enum Drivers{
+enum Driver{
     DYNAMIC,
     DUAL_DYNAMIC,
     PLANAR,
@@ -14,36 +15,122 @@ enum Drivers{
     TRIBID,
     QUADBRID,
 }
-//interfaces
-interface Sell{
-    double applyDiscount(double percentage);
-    void displayInfo();
+enum SoundSignature{
+    NEUTRAL,
+    BASSY,
+    BRIGHT,
+    BRIGHT_NEUTRAL,
+    V_SHAPE,
+    WARM_NEUTRAL,
 }
-//classes
-class Product implements Sell{
-    public double applyDiscount(double percentage){
-        return 0;
+enum ShippingStatus{
+    PENDING, 
+    SHIPPED, 
+    DELIVERED, 
+    CANCELLED,
+}
+
+enum CustomerRank{
+    NONE(0),
+    BASIC(10),
+    PREMIUM(15),
+    AUDIOPHILE(20);
+
+    double percentage;
+
+    CustomerRank(double discount){
+        this.percentage=discount;
     }
 
-    public void displayInfo(){
-        
+    public double getPercentage(){
+        return this.percentage;
+    }
+}
+
+//interfaces
+interface Sellable{
+    double applyDiscount(double percentage, CustomerRank rank);
+    double getPrice();
+}
+
+//classes
+abstract class Product implements Sellable{
+    protected double price;
+    protected String name;
+    protected String brand;
+
+    Product(String name, double price, String brand){
+        this.price=price;
+        this.name=name;
+        this.brand=brand;
+    }
+    @Override
+    public double applyDiscount(double percentage, CustomerRank rank){
+        double discount = percentage+rank.getPercentage();
+
+        if (percentage<0 || percentage>100){
+            System.out.println("Discount percentage must be between 0 and 100");
+            return this.price;
+        }
+
+        if (discount > 100){
+            discount=100;
+        }
+
+        return this.price - (this.price * (discount) / 100);
+    }
+
+    @Override
+    public double getPrice() {
+        return this.price;
+    }
+
+    public String toString(){
+        return "Name: " + this.name + "\nPrice: " + this.price + "\nBrand: " + this.brand ;
     }
 }
 
 class InEarMonitor extends Product{
+    private Driver[] drivers;
+    private SoundSignature soundSignature;
 
+    InEarMonitor(String name, double price, Driver[] drivers, String brand, SoundSignature soundSignature){
+        super(name, price, brand);
+        this.drivers = drivers;
+        this.soundSignature=soundSignature;
+    }
+
+    @Override
+    public double applyDiscount(double percentage, CustomerRank rank) {
+        return super.applyDiscount(percentage, rank);
+    }
+    String getDrivers(){
+        String listOfDrivers ="";
+        for (Driver driver : drivers){
+            listOfDrivers+=driver;
+            listOfDrivers+=", ";
+        }
+        return listOfDrivers;
+    }
+    @Override
+    public String toString(){
+        return super.toString() + "Sound Signatures: " + this.soundSignature + "Drivers: "+ getDrivers();
+    }
 }
-
-class Accessory extends Product{
-
-}
+//think of this later, idk what second product can be
+// class DigitalAudioConverter extends Product{
+//     int 
+// }
 
 class Customer{
-
+    String name;
+    CustomerRank rank;
+    ArrayList<Order> orders;
 }
 
 class Order{
-
+    Product[] cart;
+    
 }
 
 class Store{
